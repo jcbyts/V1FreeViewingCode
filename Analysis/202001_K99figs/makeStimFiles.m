ceb1fd501bbd1344eac2eafafa133ce1a5306547
Jake <jacoby8s@gmail.com>
2020-01-21 21:59:55 -0500

    scatteredInterpolant correction

--- Analysis/202001_K99figs/makeStimFiles.m
+++ Analysis/202001_K99figs/makeStimFiles.m
@@ -1,7 +1,7 @@
 
 %% add paths
 
-user = 'jakework';
+user = 'jakelaptop';
 addFreeViewingPaths(user);
 
 
@@ -12,7 +12,17 @@ sessId = 12;
 
 %% check that the session looks reasonable
 % sessionQAfigures(Exp, S)
+% eyePos1 = io.getCorrectedEyePosFixCalib(Exp, 'plot', true, 'usePolynomial', true);
+% eyePos2 = io.getCorrectedEyePosFixCalib(Exp, 'plot', true);
 
+% %%
+% figure(1); clf
+% 
+% plot(eyePos1(:,1), eyePos2(:,1), '.'); hold on
+% xlabel('Old Calibration')
+% ylabel('New Calibration')
+% plot(xlim, [0 0])
+% plot([0 0], ylim)
 %% try correcting the calibration using FixCalib protocol
 eyePos = io.getCorrectedEyePosFixCalib(Exp, 'plot', true);
 
@@ -22,6 +32,29 @@ eyePos(:,2) = sgolayfilt(eyePos(:,2), 3, 9);
 
 Exp.vpx.smo(:,2:3) = eyePos;
 
+%% use correction grid
+if exist('Data/Marmo20191231_eyecor1.mat', 'file') && sessId ==12
+    ec = load('Data/Marmo20191231_eyecor1.mat');
+    ppd = Exp.S.pixPerDeg;
+    GridCentersX = ec.corr_list(:,1) / ppd;
+    GridCentersY = ec.corr_list(:,2) / ppd;
+    CorrectionX = GridCentersX + ec.corr_list(:,3) / ppd;
+    CorrectionY = GridCentersY + ec.corr_list(:,4) / ppd;
+    
+    % learn interpolatant function
+    Fx = scatteredInterpolant(CorrectionX, CorrectionY, GridCentersX);
+    Fy = scatteredInterpolant(CorrectionX, CorrectionY, GridCentersY);
+    
+    % only fix the central points
+    iix = hypot(eyePos(:,1), eyePos(:,2)) < 5*ppd;
+    
+    eyePos2 = eyePos;
+    
+    eyePos2(iix, 1) = Fx(eyePos(iix,1), eyePos(iix,2));
+    eyePos2(iix, 2) = Fy(eyePos(iix,1), eyePos(iix,2));
+    
+    Exp.vpx.smo(:,2:3) = eyePos2;
+end
 %% regenerate data with the following parameters
 eyesmoothing = 9;
 t_downsample = 2;
