5518d911f7e3d0930a8a14700ff5c69acc8abfc0
Jake <jacoby8s@gmail.com>
2020-04-20 21:53:24 -0400

    updated datasets csv

--- MatlabCode/binTimesFast.m
+++ MatlabCode/binTimesFast.m
@@ -16,13 +16,14 @@ binfun = @(x) (x==0) + ceil(x / binSize);
 
 % bin spike times
 bst = binfun(st);
+bst(bst<1) = [];
 
 % bin frame times
 bft = binfun(eventTimes);
 
 % create binned spike times
 Y = sparse(bst, ones(numel(bst),1), ones(numel(bst), 1), max(max(bst),max(bft)),1);
-Y = sparse(bst, ones(numel(bst), 1), ones(numel(bst), 1));
+% Y = sparse(bst, ones(numel(bst), 1), ones(numel(bst), 1));
 
 % index in with binned frame times
 Y = Y(bft,:);
