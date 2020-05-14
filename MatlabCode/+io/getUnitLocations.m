7dc76c6bb112b1c0e00fade43d5de3d279c38c2b
Jake <jacoby8s@gmail.com>
2020-05-14 17:30:26 -0400

    some updates to getting the location of units / unit templates

--- MatlabCode/+io/getUnitLocations.m
+++ MatlabCode/+io/getUnitLocations.m
@@ -1,6 +1,13 @@
-function [x, y] = getUnitLocations(osp, pow)
-% [x, y] = getUnitLocations(Exp)
-
+function S = getUnitLocations(osp, pow)
+% S = getUnitLocations(osp)
+% Input:
+%   osp [struct] - getSpikesFromKilo output
+%   pow [double] - soft max power    
+% Output:
+%   S [struct]
+%       x 
+%       y 
+%       templates
 if nargin < 2
     pow = 2;
 end
@@ -10,13 +17,22 @@ NC = numel(osp.cids);
 x = nan(NC,1);
 y = nan(NC,1);
 
+nLags = size(osp.temps,2);
+nChan = size(osp.temps,3);
 
+templates = zeros(nLags, nChan, NC);
 for cc = 1:NC
     tempix = unique(osp.spikeTemplates(osp.clu==osp.cids(cc)))+1;
 
     unitTemp = squeeze(mean(osp.tempsUnW(tempix, :, :),1));
+    templates(:,:,cc) = unitTemp;
+    
     u = sum(unitTemp.^2,1);
     w = u.^pow /sum(u(:).^pow);
     x(cc) = w*osp.xcoords;
     y(cc) = w*osp.ycoords;
 end
+
+S.x = x;
+S.y = y;
+S.templates = templates;
