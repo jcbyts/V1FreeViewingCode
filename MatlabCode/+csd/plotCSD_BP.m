ae6664e67a1327b2fd89407dcfb89e5ec823ed0d
Gabe Sarch <gabrielsarch@gmail.com>
2020-06-18 22:59:28 -0400

    Improved plotting functions for visualizing BP on CSD

--- MatlabCode/+csd/plotCSD_BP.m
+++ MatlabCode/+csd/plotCSD_BP.m
@@ -1,55 +1,87 @@
-function plotCSD_BP(stats, gamma)
-% gets the event times of the current source density trials
+function plotCSD_BP(stats, gamma, onlyPlotOneShank)
+% plots gamam power landmarks (such as input layer prediction) on top  of
+% CSD for verification
 % Inputs:
 %   stats              [struct] - stats struct from csd.getCSD
+%   gamma              [struct] - gamma from csd.getGamma
+%   onlyPlotOneShank   true/false - only plots one shank if more than one
+%   (default = false)
 % 
 % ghs wrote it 2020
 
-    numShanks = size(stats.CSD, 3);
+    if nargin<3
+        onlyPlotOneShank = false;
+    end
+    
+    if onlyPlotOneShank 
+        numShanks = 1;
+    else
+        numShanks = size(stats.CSD, 3);
+    end
     
 
     if numShanks == 1
-        figure; clf;
-        imagesc(stats.time, stats.depth, stats.CSD-mean(stats.CSD(:))); axis ij
-        colormap(colormap_redblackblue);
+        %figure; clf;
+        curCSD = stats.CSD(:,:,1);
+        imagesc(stats.time, stats.depth, curCSD-mean(curCSD(:))); axis ij
+        %colormap(colormap_redblackblue);
         hold on
-        plot(stats.time, bsxfun(@plus, stats.STA, stats.chDepths), 'Color', repmat(.1, 1, 3))
+        plot(stats.time, bsxfun(@plus, stats.STA(:,:,1), stats.chDepths), 'Color', repmat(.1, 1, 3))
         xlim(stats.time([1 end]))
         %plot(stats.time([1 end]), stats.sinkDepth(1)*[1 1], 'w--', 'Linewidth', 2)
-        plot(stats.time([1 end]), [1; 1]*stats.reversalPointDepth{1}, 'r--', 'Linewidth', 2)
-        tmp = abs(stats.reversalPointDepth{1} - stats.sinkDepth(1));
-        tmp = tmp + stats.sinkDepth(1);
-        plot(stats.time([1 end]), [1; 1]*tmp, 'r--', 'Linewidth', 2)
+        h3 = plot(stats.time([1 end]), [1; 1]*stats.reversalPointDepth{1}(1), 'r--', 'Linewidth', 2);
+        %tmp = abs(stats.reversalPointDepth{1} - stats.sinkDepth(shankInd));
+        %tmp = tmp + stats.sinkDepth(shankInd);
+        %plot(stats.time([1 end]), [1; 1]*tmp, 'r--', 'Linewidth', 2)
+        
+        xLimits = get(gca,'XLim');
+        scale = xLimits(2);
+        h1 = plot(gamma.lgPower(:,1).*scale, stats.chDepths, 'Color', 'black', 'Linewidth', 1);
+        %h(2) = plot(gamma.hgPower(:,shankInd).*scale, stats.chDepths, 'Color', 'green', 'Linewidth', 1);
+        
+        h2 = plot(stats.time([1 end]), [1; 1]*gamma.lgInputLayerDepths(:,:,1), 'black--', 'Linewidth', 2);
+        %plot(stats.time([1 end]), [1; 1]*gamma.lgMinDepth(shankInd), 'w--', 'Linewidth', 2)
+        
         %                 axis ij
+        xlabel('Time (ms)')
+        ylabel('Depth (nm)')
+        title(['Shank #' num2str(1)])
         hold off
-        colorbar
     elseif numShanks > 1
-        figure; clf;
+        %figure; clf;
         for shankInd = 1:numShanks
         %curShankInds = shankInd*lenShanks-lenShanks+1:shankInd*lenShanks;
         curCSD = stats.CSD(:,:,shankInd);
         subplot(1,numShanks,shankInd)
         imagesc(stats.time, stats.depth, curCSD-mean(curCSD(:))); axis ij
-        colormap(colormap_redblackblue);
+        %colormap(colormap_redblackblue);
         hold on
         plot(stats.time, bsxfun(@plus, stats.STA(:,:,shankInd), stats.chDepths), 'Color', repmat(.1, 1, 3))
         xlim(stats.time([1 end]))
         %plot(stats.time([1 end]), stats.sinkDepth(shankInd)*[1 1], 'w--', 'Linewidth', 2)
-        plot(stats.time([1 end]), [1; 1]*stats.reversalPointDepth{shankInd}(1), 'r--', 'Linewidth', 2)
-        tmp = abs(stats.reversalPointDepth{1} - stats.sinkDepth(shankInd));
-        tmp = tmp + stats.sinkDepth(shankInd);
+        h3 = plot(stats.time([1 end]), [1; 1]*stats.reversalPointDepth{shankInd}(1), 'r--', 'Linewidth', 2);
+        %tmp = abs(stats.reversalPointDepth{1} - stats.sinkDepth(shankInd));
+        %tmp = tmp + stats.sinkDepth(shankInd);
         %plot(stats.time([1 end]), [1; 1]*tmp, 'r--', 'Linewidth', 2)
         
         xLimits = get(gca,'XLim');
         scale = xLimits(2);
-        plot(gamma.lgPower(:,shankInd).*scale, stats.chDepths, 'Color', 'white', 'Linewidth', 1)
-        plot(gamma.hgPower(:,shankInd).*scale, stats.chDepths, 'Color', 'green', 'Linewidth', 1)
-        plot(stats.time([1 end]), [1; 1]*gamma.lgMinDepth(shankInd), 'w--', 'Linewidth', 2)
+        h1 = plot(gamma.lgPower(:,shankInd).*scale, stats.chDepths, 'Color', 'black', 'Linewidth', 1);
+        %h(2) = plot(gamma.hgPower(:,shankInd).*scale, stats.chDepths, 'Color', 'green', 'Linewidth', 1);
+        
+        h2 = plot(stats.time([1 end]), [1; 1]*gamma.lgInputLayerDepths(:,:,shankInd), 'black--', 'Linewidth', 2);
+        %plot(stats.time([1 end]), [1; 1]*gamma.lgTroughDepth(shankInd), 'w--', 'Linewidth', 2)
         
         %                 axis ij
+        xlabel('Time (ms)')
+        ylabel('Depth (nm)')
+        title(['Shank #' num2str(shankInd)])
         hold off
-        colorbar
+
         end
+        
     end
+    colorbar
+        legend([h3; h1; h2], 'CSD reversal point', 'low gamma power', 'low G input layer depths')
 end
 
--- MatlabCode/+csd/visualizeLayers.m
+++ MatlabCode/+csd/visualizeLayers.m
@@ -4,14 +4,15 @@ function visualizeLayers(sessNums)
 %   input:
 %        sessNums          [1xnumSessions] vector of session numbers
 
+method = 'softmax';
+
 dataPath = getpref('FREEVIEWING', 'PROCESSED_DATA_DIR');
 
 meta_file = fullfile(fileparts(which('addFreeViewingPaths')), 'Data', 'datasets.csv');
 
 data = readtable(meta_file);
 
-shankLen = 32; % number of channels on each shank
-numPlots = sum(table2array(data(sessNums, 16)))/shankLen;
+numPlots = length(sessNums);
 
 di = 1:numPlots;
 di = di(~(rem(numPlots, di)));
@@ -28,60 +29,20 @@ h = gcf; clf;
 gap = 0.005;
 ha = csd.tight_subplot(num_vert,num_hor, [0.04 gap],[gap 0.04],[gap gap]);
 
-plotInd = 1;
 for indSess = 1:length(sessNums)
     [Exp, ~, lfp] = io.dataFactoryGratingSubspace(sessNums(indSess));
     
-    stats = csd.getCSD(lfp, Exp, 'window', [-100 200], 'plot', false, 'method',...
-        'spline', 'sampleRate', 1000, 'exclude', true); % compute CSD
+    axes(ha(indSess))
+    csd.plotGammaLayerPrediction(lfp, Exp, 'method', method, 'onlyPlotOneShank', true)
     
-    gamma = csd.getGamma(lfp);
+    colorbar off
     
-    numShanks = size(stats.CSD, 3);
-    for shankInd = 1:numShanks
-        
-        axes(ha(plotInd))
-        
-        curCSD = stats.CSD(:,:,shankInd);
-        imagesc(stats.time, stats.depth, curCSD-mean(curCSD(:))); axis ij
-        colormap(colormap_redblackblue);
-        hold on
-        plot(stats.time, bsxfun(@plus, stats.STA(:,:,shankInd), stats.chDepths), 'Color', repmat(.1, 1, 3))
-        xlim(stats.time([1 end]))
-        %plot(stats.time([1 end]), stats.sinkDepth(shankInd)*[1 1], 'w--', 'Linewidth', 2)
-        if ~isempty(stats.reversalPointDepth{shankInd})
-            plot(stats.time([1 end]), [1; 1]*stats.reversalPointDepth{shankInd}, 'r--', 'Linewidth', 2)
-        end
-        tmp = abs(stats.reversalPointDepth{1} - stats.sinkDepth(shankInd));
-        tmp = tmp + stats.sinkDepth(shankInd);
-        if ~isempty(tmp)
-            plot(stats.time([1 end]), [1; 1]*tmp, 'r--', 'Linewidth', 2)
-        end
-        xLimits = get(gca,'XLim');
-        scale = xLimits(2);
-        shankInds = (shankLen*(shankInd-1)+1):(shankLen*shankInd);
-        
-        plot(gamma.lgPower(:,shankInd).*scale, stats.chDepths, 'Color', 'white', 'Linewidth', 1)
-        %plot(gamma.hgPower(:,shankInd).*scale, stats.chDepths, 'Color', 'green', 'Linewidth', 1)
-        plot(stats.time([1 end]), [1; 1]*gamma.lgMinDepth(shankInd), 'white', 'Linewidth', 2)
-        %plot(stats.time([1 end]), [1; 1]*gamma.lgInputLayerDepths(:,:,shankInd), 'white', 'Linewidth', 2)
-        %plot(stats.time([1 end]), [1; 1]*gamma.hgInputLayerDepths(:,:,shankInd), 'green', 'Linewidth', 2)
-
-        
-        hold off
-        
-        %yticks(35:35:1120)
-        %yticklabels({'1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31', '32'})
-        
-        if ~(indSess==length(sessNums))
-            set(gca,'xticklabel',{[]})
-            set(gca,'yticklabel',{[]})
-        end
-        title(strcat(string(data{sessNums(indSess), 2}), '-', string(data{sessNums(indSess), 1})))
-        
-        plotInd = plotInd + 1;
-        
+    if ~(indSess==length(sessNums))
+        set(gca,'xticklabel',{[]})
+        set(gca,'yticklabel',{[]})
     end
+    title(strcat(string(data{sessNums(indSess), 2}), '-', string(data{sessNums(indSess), 1})))
+    
     
 end
 
