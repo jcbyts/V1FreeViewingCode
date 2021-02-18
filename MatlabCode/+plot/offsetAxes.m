c78a8311383a29ebc363bc3f754e4621fba4d36e
Jake <jacoby8s@gmail.com>
2021-02-17 20:16:20 -0500

    offsetAxes plotting function is more customizable

--- MatlabCode/+plot/offsetAxes.m
+++ MatlabCode/+plot/offsetAxes.m
@@ -1,16 +1,20 @@
-function offsetAxes(ax)
+function offsetAxes(ax, yonly, amount)
 % thanks to Pierre Morel, undocumented Matlab
 % and https://stackoverflow.com/questions/38255048/separating-axes-from-plot-area-in-matlab
 %
 % by Anne Urai, 2016
 
 if ~exist('ax', 'var'), ax = gca; end
+if ~exist('yonly', 'var'), yonly = false; end
+if ~exist('amount', 'var'), amount = 4; end
 
 set(ax, 'TickDir', 'out', 'Box', 'off')
 
 % modify the x and y limits to below the data (by a small amount)
-ax.XLim(1) = ax.XLim(1)-(ax.XTick(2)-ax.XTick(1))/4;
-ax.YLim(1) = ax.YLim(1)-(ax.YTick(2)-ax.YTick(1))/4;
+if ~yonly
+ax.XLim(1) = ax.XLim(1)-(ax.XTick(2)-ax.XTick(1))/amount;
+end
+ax.YLim(1) = ax.YLim(1)-(ax.YTick(2)-ax.YTick(1))/amount;
 
 % this will keep the changes constant even when resizing axes
 addlistener (ax, 'MarkedClean', @(obj,event)resetVertex(ax));
