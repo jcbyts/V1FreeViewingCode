251f9e1d5aa7ac5cb9c821dc7f25aec68f1f402f
Jake <jacoby8s@gmail.com>
2022-05-02 21:48:36 -0700

    tent_basis updated to handle asymmetric tents

--- MatlabCode/tent_basis.m
+++ MatlabCode/tent_basis.m
@@ -1,14 +1,20 @@
 function B = tent_basis(y, ctrs)
-    if numel(ctrs)==1
-        dc = ctrs;
-        dscale1 = dc;
-        dscale2 = dc;
+% B = tent_basis(y, ctrs)
+
+if numel(ctrs)==1
+    dc = ctrs;
+    dscale1 = dc;
+    dscale2 = dc;
+else
+    dc = diff(ctrs);
+    if ctrs(1)==0
+        dscale1 = [dc(1) dc];
     else
-        dc = diff(ctrs);
         dscale1 = [ctrs(1) dc];
-        dscale2 = [dc dc(end)];
     end
-    
-    ydiff = y(:) - ctrs;
-    yd = (ydiff .* (ydiff<0) ./ dscale1) + (ydiff .* (ydiff>0) ./ dscale2);
-    B = max(1-abs(yd), 0);
+    dscale2 = [dc dc(end)];
+end
+
+ydiff = y(:) - ctrs;
+yd = (ydiff .* (ydiff<0) ./ dscale1) + (ydiff .* (ydiff>0) ./ dscale2);
+B = max(1-abs(yd), 0);
